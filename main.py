b385e6773b1f93edcbbada2c11f443af9ef644cb
oleg-nai <o.naidovich@gmail.com>
2023-07-31 10:59:34 +0300

    Updated TODO and README

--- main.py
+++ main.py
@@ -16,9 +16,6 @@ extra = os.environ.get("extra")
 source = os.environ.get("camera_url")
 server_url = os.environ.get("server_url")
 
-# TODO: wrong status server_url status, change it...
-# TODO: and change the same in model_image...
-
 
 logger = create_logger()
 dataset = HTTPLIB2Capture(source, username=username, password=password)
